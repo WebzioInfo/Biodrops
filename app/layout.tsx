e487607b80f942d0ea682c119e58afbd4f4e08a0
Webzio Info <webzio.info@gmail.com>
2026-09-11 03:01:32 -0700

    fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,16 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "Biofix Technology LLP" }],
   creator: "Biofix Technology LLP",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
   openGraph: {
     type: "website",
     locale: "en_IN",
@@ -87,6 +97,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${archivo.variable} ${spaceGrotesk.variable}`} suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
         <link rel="author" href="/humans.txt" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <script
